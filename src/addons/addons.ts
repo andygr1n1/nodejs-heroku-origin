54b659bc3441def06fe4392d2fd4e94c4586d78e
Grini Andrei <Andrei.Grini@carasent.com>
2022-12-18 22:01:00 +0200

    file upload

--- src/addons/addons.ts
+++ src/addons/addons.ts
@@ -3,12 +3,15 @@ import express, { Express } from 'express'
 import bodyParser from 'body-parser'
 import { errorHandling } from '../utils/errorHandling.js'
 import 'express-async-errors'
+import fileUpload from 'express-fileupload'
 
 export const useAddons = (app: Express) => {
     app.use(express.json())
     app.use(cors())
     app.use(bodyParser.json())
-    app.use('/', express.static('public'))
+    app.use(fileUpload())
+    app.use('/public', express.static('public'))
+    app.use('/public/kzen-img', express.static('public/kzen-img'))
 }
 
 export const useErrorHandling = (app: Express) => {
