c05577e2225546bdfa0023464983cc9ce2a3a9d7
Andrew <andy.grini@gmail.com>
2023-06-04 11:24:40 +0100

    Feature/kzen basic login (#6)
    * Added user login route for KZen
    ---------
    
    Co-authored-by: Grini Andrei <andrei.grini@carasent.com>

--- src/routes/kzen/KZenRoute.enum.ts
+++ src/routes/kzen/KZenRoute.enum.ts
@@ -2,4 +2,5 @@ export enum KZEN_ROUTE_ENUM {
     IMG_UPLOAD = '/kzen/file-upload',
     IMG_REMOVE = '/kzen/file-remove',
     AUTO_RITUALIZE_GOALS = '/kzen/auto-ritualize-goals',
+    LOGIN = '/kzen/login',
 }
