12b9b6a4c6526db9e3edbcf79fa6d7c7562fd60f
Andrew <andy.grini@gmail.com>
2023-07-16 20:32:11 +0100

    upload sprint image (#9)
    Co-authored-by: Grini Andrei <andrei.grini@carasent.com>

--- src/routes/kzen/KZenRoute.enum.ts
+++ src/routes/kzen/KZenRoute.enum.ts
@@ -3,6 +3,7 @@ export enum KZEN_ROUTE_ENUM {
     IMG_REMOVE = '/kzen/file-remove',
     IMG_UPLOAD_AVATAR = '/kzen/file-upload-avatar',
     IMG_REMOVE_AVATAR = '/kzen/file-remove-avatar',
+    IMG_UPLOAD_SPRINT = '/kzen/upload-sprint-image',
     AUTO_RITUALIZE_GOALS = '/kzen/auto-ritualize-goals',
     LOGIN = '/kzen/login',
     REGISTER = '/kzen/register',
