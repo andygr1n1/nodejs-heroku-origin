b96b8b4655b059d74902a91723286e7a13d8afac
Grini Andrei <andrei.grini@carasent.com>
2024-03-09 13:00:27 +0000

    canceld autoritualize

--- src/routes/kzen/auto-ritualize-goals/kZenAutoRitualizeGoal.route.ts
+++ src/routes/kzen/auto-ritualize-goals/kZenAutoRitualizeGoal.route.ts
@@ -1,8 +1,8 @@
 import type { Express } from 'express'
 import { KZEN_ROUTE_ENUM } from '../KZenRoute.enum.js'
-import { fetchExpiredRitualsQuery } from './helpers/fetchExpiredRituals.query.js'
-import { generateNewRitualCircle } from './helpers/generateNewRitualCircle.helper.js'
-import { updateRitualGoal } from './helpers/updateRitualGoals.mutation.js'
+// import { fetchExpiredRitualsQuery } from './helpers/fetchExpiredRituals.query.js'
+// import { generateNewRitualCircle } from './helpers/generateNewRitualCircle.helper.js'
+// import { updateRitualGoal } from './helpers/updateRitualGoals.mutation.js'
 
 export const kZenAutoRitualizeGoal = (app: Express) => {
     app.post(KZEN_ROUTE_ENUM.AUTO_RITUALIZE_GOALS, async function (req, res) {
@@ -17,18 +17,19 @@ export const kZenAutoRitualizeGoal = (app: Express) => {
 }
 
 export async function updateExpiredRituals() {
-    const expiredRitualGoals = await fetchExpiredRitualsQuery()
-    console.info('expiredRitualGoals', expiredRitualGoals)
+    return
+    // const expiredRitualGoals = await fetchExpiredRitualsQuery()
+    // console.info('expiredRitualGoals', expiredRitualGoals)
 
-    expiredRitualGoals?.forEach((goal) => {
-        const { ritual_goal_created_at, ritual_goal_finished_at } = generateNewRitualCircle({
-            ritual_type: goal.goal_ritual.ritual_type,
-            new_ritual_interval: goal.goal_ritual.ritual_interval,
-            goal_created_at: new Date(goal.created_at),
-            goal_finished_at: new Date(goal.finished_at),
-        })
+    // expiredRitualGoals?.forEach((goal) => {
+    //     const { ritual_goal_created_at, ritual_goal_finished_at } = generateNewRitualCircle({
+    //         ritual_type: goal.goal_ritual.ritual_type,
+    //         new_ritual_interval: goal.goal_ritual.ritual_interval,
+    //         goal_created_at: new Date(goal.created_at),
+    //         goal_finished_at: new Date(goal.finished_at),
+    //     })
 
-        const goalToUpdate = { id: goal.id, created_at: ritual_goal_created_at, finished_at: ritual_goal_finished_at }
-        updateRitualGoal(goalToUpdate)
-    })
+    //     const goalToUpdate = { id: goal.id, created_at: ritual_goal_created_at, finished_at: ritual_goal_finished_at }
+    //     updateRitualGoal(goalToUpdate)
+    // })
 }
