a425a5cb8eca3941bd7d7c6fb18451feab3665c8
Grini Andrei <andrei.grini@carasent.com>
2024-12-03 09:18:46 +0200

    feat: Debugger

--- src/services/graphql-service/index.ts
+++ src/services/graphql-service/index.ts
@@ -4,6 +4,7 @@ export const generateClient = (): GraphQLClient | undefined => {
     try {
         const endpoint = process.env.VITE_CLIENT_ENDPOINT
         const secret = process.env.VITE_X_HASURA_ADMIN_SECRET
+        console.log('endpoint and secret', endpoint, secret)
         if (!endpoint || !secret) throw new Error('graphql-request: generateClient error - env errors')
         const client = new GraphQLClient(endpoint, {
             headers: { 'x-hasura-admin-secret': secret },
