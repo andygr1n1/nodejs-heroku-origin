61bad4ca633d1a728c7c5ff9ee5d028f2a3c9e94
Andrei Grini <andy.grini@gmail.com>
2025-08-31 08:48:26 +0000

    include local network

--- src/addons/index.ts
+++ src/addons/index.ts
@@ -33,7 +33,9 @@ export const useAddons = (app: Express) => {
                 try {
                     const { hostname } = new URL(origin)
                     const isLocalhost = hostname === 'localhost' || hostname === '127.0.0.1' || hostname === '::1'
-                    if (isLocalhost) {
+                    // Also allow local network IPs (192.168.x.x, 10.x.x.x, 172.16-31.x.x)
+                    const isLocalNetwork = /^(192\.168\.|10\.|172\.(1[6-9]|2[0-9]|3[0-1])\.)/.test(hostname)
+                    if (isLocalhost || isLocalNetwork) {
                         callback(null, true)
                         return
                     }
