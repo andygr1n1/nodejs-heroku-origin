f0ff648e7ed58b4c271ba4f8cece84429b5efe0e
andygr1n1 <andy.grini@gmail.com>
2026-05-03 16:03:08 +0100

    cors fix

--- src/addons/index.ts
+++ src/addons/index.ts
@@ -5,10 +5,19 @@ import express, { type Express } from 'express'
 import 'express-async-errors'
 import fileUpload from 'express-fileupload'
 
+const corsOptions: cors.CorsOptions = {
+    credentials: true,
+    origin: (_origin, callback) => callback(null, true),
+    methods: ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-API-Key', 'x-api-key', 'Cookie'],
+    optionsSuccessStatus: 204,
+}
+
 export const useAddons = (app: Express) => {
+    /* CORS first so preflight (OPTIONS) is answered before body parsing / routes */
+    app.use(cors(corsOptions))
     app.use(express.json({ limit: '5mb' }))
     app.use(bodyParser.json())
-    app.use(cors({ credentials: true, origin: (_origin, callback) => callback(null, true) }))
     app.use(fileUpload({ limits: { fileSize: 100 * 1024 * 1024 } }) as unknown as express.RequestHandler)
     app.use('/public', express.static('public'))
     app.use(cookieParser())
